db6b56fcfa7359e2b719598be60828d13876292a
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-07 17:44:26 +0545

    add signin route

--- server/server.js
+++ server/server.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser");
+
+// body-parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+const database = {
+  users: [
+    {
+     id: "123",
+     name: "bipin",
+     email: "bipin@gmail.com",
+     password: "shrestha",
+     entries: 0,
+     joined: new Date()
+    }
+  ]
+}
 
 app.get("/", (req, res) => {
   res.send("This is home route");
 });
+
+app.post("/signin", (req, res) => {
+  if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+    res.json("success");
+  } else {
+    res.status(400).json("not match");
+  }
+})
+
 
 
 const port = process.env.PORT || 5000;

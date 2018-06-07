46fbd34549d656c270ed70e193e002280cc45e3d
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-07 09:55:58 +0545

    add Rank

---
+++ client/src/components/Rank.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+ const Rank =() => {
+  return (
+    <div className="container mt-5">
+      <p className="lead">
+      {"Bipin, your current rank is "}
+      {"#4"}
+      </p>
+    </div>
+  )
+}
+
+export default Rank;

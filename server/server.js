ef98b58b6e64b765119333a13a9e964133db6fde
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-07 18:13:28 +0545

    add profile route

--- server/server.js
+++ server/server.js
@@ -5,33 +5,45 @@ const bodyParser = require("body-parser");
 
 // body-parser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const database = {
   users: [
     {
-     id: "1234",
-     name: "bipin",
-     email: "bipin@gmail.com",
-     password: "shrestha",
-     entries: 0,
-     joined: new Date()
+      id: "1234",
+      name: "bipin",
+      email: "bipin@gmail.com",
+      password: "shrestha",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: "4321",
+      name: "sudip",
+      email: "sudip@gmail.com",
+      password: "shrestha",
+      entries: 0,
+      joined: new Date()
     }
   ]
 }
 
+// index/home route: GET
 app.get("/", (req, res) => {
   res.json(database.users);
 });
 
+
+// sign in route: POST
 app.post("/signin", (req, res) => {
-  if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
     res.json("success");
   } else {
     res.status(400).json("not match");
   }
 })
 
+// register route: POST
 app.post("/register", (req, res) => {
   const newUser = {
     id: "4433",
@@ -42,8 +54,25 @@ app.post("/register", (req, res) => {
     joined: new Date()
   }
   database.users.push(newUser);
-  res.json(database.users[database.users.length-1]);
+  res.json(database.users[database.users.length - 1]);
 });
+
+// profile route: GET
+app.get("/profile/:id", (req, res) => {
+  const id = req.params.id;
+  let found= false;
+  database.users.map(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if(!found) {
+    res.status(400).json("User not found");
+  }
+});
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
